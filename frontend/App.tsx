46ccc329416c66327bb189d5a375f39a3cc09e12
Asher Lozano <92909796+asherlozano@users.noreply.github.com>
2023-01-24 16:09:17 -0800

    Update App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -19,7 +19,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>{msg}</Text>
+      <Text>{FitSocial}</Text>
       <StatusBar style="auto" />
     </View>
   );
